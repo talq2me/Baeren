73842612fff76b7210c4314d81cc7d10fb5bc904
talq2me <tiffany.quinlan@gmail.com>
2025-03-28 17:40:17 -0400

    Update videoModal.js

--- videoModal.js
+++ videoModal.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var videoData = {};
 
     // Load video data from JSON file
-    fetch("../../videoLinks.json")
+    fetch("/Baeren/videoLinks.json")
         .then(response => response.json())
         .then(data => {
             videoData = data;
