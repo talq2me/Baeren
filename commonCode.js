5b77d69a667984174486492bb387092d87b45193
talq2me <tiffany.quinlan@gmail.com>
2025-06-11 16:56:58 -0400

    param revert

--- commonCode.js
+++ commonCode.js
@@ -33,7 +33,7 @@ function rewardworking(timeInMins) {
 function reward(timeInMins) {
     const rewardTime = timeInMins * 60 * 1000;
     if (typeof fully !== "undefined") {
-        fully.startApplication("com.talq2me.netflixreward", { minutes: timeInMins.toString() }); // Launch Netflix Reward app
+        fully.startApplication("com.talq2me.netflixreward");//, { minutes: timeInMins.toString() }); // Launch Netflix Reward app
 
     } else {
         console.warn("Fully Kiosk is not available.");
